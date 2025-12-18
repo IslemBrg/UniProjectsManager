b90592b2cc408ef84a412da67e89a14e3aa78dfb
Islem Bargaoui <islem.bargaoui@dibsgo.com>
2025-12-18 20:01:10 +0100

    🔧 Update settings to use environment variables for configuration; added dotenv support for better security and flexibility. and migrated to  neon postgresql

--- uniprojects/settings.py
+++ uniprojects/settings.py
@@ -4,21 +4,24 @@ Django settings for UniProjectsManager project.
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-replace-this-in-production-#$%^&*()_+-=[]{}|:;<>,.?/'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
@@ -71,10 +74,9 @@ WSGI_APPLICATION = 'uniprojects.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
